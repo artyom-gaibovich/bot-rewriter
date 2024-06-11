1c61b1721be53030df00283193891f72cbfe82c5
ArtyomIzmaylov <artyomguybov2002@gmail.com>
2024-06-11 20:31:27 +0300

    DI почти сделан, только есть проблемка с конфгурацией

--- src/telegram-bot/scenes/delete-user-channel/delete-user-channel-scene.module.ts
+++ src/telegram-bot/scenes/delete-user-channel/delete-user-channel-scene.module.ts
@@ -1,8 +1,20 @@
 import {Module} from "@nestjs/common";
 import {ChannelManagerModule} from "../../../manager/channel/channel.manager.module";
+import {DeleteUserChannelScene} from "./delete-user-channel-scene";
+import {ChannelManagerInterface} from "../../../manager/channel/channel.manager.interface";
 
 @Module({
-    imports : [ChannelManagerModule]
+    imports : [ChannelManagerModule],
+    providers : [
+        {
+            provide : DeleteUserChannelScene,
+            useFactory : (channelManager : ChannelManagerInterface) => {
+                return new DeleteUserChannelScene(channelManager)
+            },
+        }
+    ],
+    exports: [DeleteUserChannelScene]
+
 })
 export class DeleteUserChannelSceneModule {
 

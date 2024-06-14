73945254bd946dd5025ccf7b7ddf47541dd7ad42
ArtyomIzmaylov <artyomguybov2002@gmail.com>
2024-06-14 20:16:52 +0300

    сделал контейнеризаци

--- Makefile
+++ Makefile
@@ -1,1 +1,16 @@
-start:
+ifneq ("$(wildcard .env)","")
+    include .env
+endif
+
+
+up:
+	@docker compose up -d --remove-orphans --build --force-recreate
+
+down:
+	@docker compose down --remove-orphans
+
+exec:
+	@docker compose exec -u root app sh
+
+restart:
+	make down && make up
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import {AppModule} from "./app.module";
+import {ConfigService} from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+  const config = new ConfigService()
+  await app.listen(parseInt(config.get('APP_PORT')));
 }
 bootstrap();

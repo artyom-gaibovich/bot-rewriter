5cfcf144f5e269b29eb036f045a077de01375c3a
ArtyomIzmaylov <artyomguybov2002@gmail.com>
2024-06-16 23:33:05 +0300

    рабочий вариант, но нужен рефакторинг

--- src/telegram-bot/pages/main-channel/add-user-channel/add-channel-category/add-channel-category.ts
+++ src/telegram-bot/pages/main-channel/add-user-channel/add-channel-category/add-channel-category.ts
@@ -1,76 +1,98 @@
 import {
     ADD_CHANNEL_CATEGORY,
-    ADD_USER_CHANNEL_PAGE,
-    MAIN_CHANNEL_PAGE,
-    MAIN_CHANNELS_TO_REWRITE_PAGE
 } from "../../../pages.types";
 import {AddStep, Ctx, Scene, SceneEnter} from "nestjs-puregram";
 import {Inject} from "@nestjs/common";
-import {LinkValidatorInterface} from "../../../../../validator/link.validator.interface";
-import {ChannelManagerInterface} from "../../../../../manager/channel/channel.manager.interface";
-import {AddUserChannelSceneContext} from "../add-user-channel";
 import {CategoryRepositoryInterface} from "../../../../../repository/category/category.repository.interface";
 import {TelegramContextModel} from "../../../../model/telegram-context-model";
 import {StepContext} from "@puregram/scenes";
-import {ChannelLinkInterface} from "../../../../../model/link/channel.link.interface";
-import {UserChannelInterface} from "../../../../../model/channel.interface";
-
 
 export interface AddChannelCategoryInterface extends Record<string, any> {
-    categories : CategoryInterface[]
-    limit : number
+    categories: CategoryInterface[];
+    limit: number;
+    currentPage: number;
 }
 
-export type AddChannelCategoryContext = TelegramContextModel & StepContext<AddChannelCategoryInterface>
-
-
+export type AddChannelCategoryContext = TelegramContextModel & StepContext<AddChannelCategoryInterface>;
 
 @Scene(ADD_CHANNEL_CATEGORY)
 export class AddChannelCategory {
     constructor(
         @Inject('CATEGORY_REPOSITORY') private repository: CategoryRepositoryInterface,
-    ) {
-    }
+    ) {}
 
     @SceneEnter()
     async sceneEnter(@Ctx() telegramContext: AddChannelCategoryContext) {
-        telegramContext.scene.state.categories = await this.repository.findAll()
-        telegramContext.scene.state.limit = 0
+        if (telegramContext.scene.step.firstTime) {
+            telegramContext.scene.state.categories = await this.repository.findAll();
+            telegramContext.scene.state.limit = 5; // specify your limit
+            telegramContext.scene.state.currentPage = 0;
+            return await this.showCategories(telegramContext);
+        }
     }
 
     @AddStep(0)
     async zeroStep(@Ctx() telegramContext: AddChannelCategoryContext) {
-        const categories = telegramContext.scene.state.categories
+        if (telegramContext.text === 'Следующая') {
+            telegramContext.scene.state.currentPage++;
+            await this.showCategories(telegramContext);
 
-        //0,5,10,15,20,25,30,35,40
-        const limit = telegramContext.scene.state.limit
-        const channelsCount = telegramContext.scene.state.userChannels.length
-        const categoriesKeyboard = categories.map(category => {
-            return [{text : category.title}]
-        })
-
-        const backKeyboard = [
-            [{text : 'Назад'}],
-        ]
-        const limitKeyboard = [
-            [{text : 'Повысить лимит'}],
-        ]
-        let mainKeyboard = []
-
-        if (channelsCount > 0 && channelsCount < limit) {
-            mainKeyboard = [...categoriesKeyboard, ...categoriesKeyboard, ...backKeyboard]
+        } else if (telegramContext.text === 'Назад') {
+            telegramContext.scene.state.currentPage--;
+            await this.showCategories(telegramContext);
+        } else {
+            // Handle category selection or exit
         }
-        if (channelsCount === 0) {
-            mainKeyboard = [...categoriesKeyboard, ...backKeyboard]
-        }
-
-        await telegramContext.send('Выберите дальнейшее действие', {
-            reply_markup : {
-                resize_keyboard : true,
-                remove_keyboard : true,
-                keyboard : [...mainKeyboard]
-            }
-        })
     }
 
+    private async showCategories(telegramContext: AddChannelCategoryContext) {
+        const { categories, limit, currentPage } = telegramContext.scene.state;
+
+        const startIndex = currentPage * limit;
+        const endIndex = startIndex + limit;
+        const categoriesForPage = categories.slice(startIndex, endIndex);
+
+        const exitKeyboard = [
+            [{ text: 'Выйти' }]
+        ];
+        const nextKeyboard = [
+            [{ text: 'Следующая' }],
+        ];
+        const backKeyboard = [
+            [{ text: 'Назад' }],
+        ];
+
+        let mainKeyboard = [];
+
+        if (currentPage === 0) {
+            mainKeyboard = [
+                ...categoriesForPage.map(category => [{ text: category.title }]),
+                ...nextKeyboard,
+            ];
+        } else {
+            if (categoriesForPage.length !== limit) {
+                mainKeyboard = [
+                    ...backKeyboard,
+                    ...categoriesForPage.map(category => [{ text: category.title }]),
+                    ...exitKeyboard,
+                ];
+            }
+            else {
+                mainKeyboard = [
+                    ...backKeyboard,
+                    ...categoriesForPage.map(category => [{ text: category.title }]),
+                    ...nextKeyboard,
+                    ...exitKeyboard,
+                ];
+            }
+        }
+        console.log(mainKeyboard)
+        await telegramContext.send('Выберите дальнейшее действие', {
+            reply_markup: {
+                resize_keyboard: true,
+                remove_keyboard: true,
+                keyboard: mainKeyboard
+            }
+        });
+    }
 }
